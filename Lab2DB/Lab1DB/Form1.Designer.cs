2021f2245a4e62f82a95ab955979a29f6d28494d
Pop Lorena Corina <plie2397@scs.ubbcluj.ro>
2019-04-17 16:07:42 +0300

    lab2

---
+++ Lab2DB/Lab1DB/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Lab1DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent() {
+            this.parentTable = new System.Windows.Forms.DataGridView();
+            this.childTable = new System.Windows.Forms.DataGridView();
+            this.viewButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.parentTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.childTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // parentTable
+            // 
+            this.parentTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.parentTable.Location = new System.Drawing.Point(15, 116);
+            this.parentTable.Margin = new System.Windows.Forms.Padding(6);
+            this.parentTable.Name = "parentTable";
+            this.parentTable.RowTemplate.Height = 23;
+            this.parentTable.Size = new System.Drawing.Size(879, 835);
+            this.parentTable.TabIndex = 0;
+            // 
+            // childTable
+            // 
+            this.childTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.childTable.Location = new System.Drawing.Point(930, 114);
+            this.childTable.Margin = new System.Windows.Forms.Padding(6);
+            this.childTable.Name = "childTable";
+            this.childTable.RowTemplate.Height = 24;
+            this.childTable.Size = new System.Drawing.Size(916, 837);
+            this.childTable.TabIndex = 1;
+            // 
+            // viewButton
+            // 
+            this.viewButton.Location = new System.Drawing.Point(1325, 38);
+            this.viewButton.Name = "viewButton";
+            this.viewButton.Size = new System.Drawing.Size(100, 51);
+            this.viewButton.TabIndex = 2;
+            this.viewButton.Text = "View";
+            this.viewButton.UseVisualStyleBackColor = true;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(1983, 631);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(113, 51);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(1983, 754);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(122, 51);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1307, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Child";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(356, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Parent";
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(1983, 871);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(130, 51);
+            this.updateButton.TabIndex = 7;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Location = new System.Drawing.Point(1875, 127);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(379, 461);
+            this.flowLayoutPanel.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2295, 1418);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.childTable);
+            this.Controls.Add(this.parentTable);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.parentTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.childTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    
+
+        #endregion
+
+    
+        private System.Windows.Forms.DataGridView parentTable;
+        private System.Windows.Forms.DataGridView childTable;
+        private System.Windows.Forms.Button viewButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+
+
+
+    }
+}
+
---
+++ Lab2DB/Lab1DB/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Lab1DB
+{
+    public partial class Form1 : Form
+    {
+        private SqlDataAdapter da = new SqlDataAdapter();
+        private DataSet ds = new DataSet();
+        private string childTableName = ConfigurationManager.AppSettings["ChildTableName"];
+        private string parentTableName = ConfigurationManager.AppSettings["ParentTableName"];
+        private string columnNamesInsertParameters = ConfigurationManager.AppSettings["ColumnNamesInsertParameters"];
+        private List<string> columnNames = new List<string>(ConfigurationManager.AppSettings["ChildLabelNames"].Split(','));
+        private List<string> paramsNames = new List<string>(ConfigurationManager.AppSettings["ColumnNamesInsertParameters"].Split(','));
+        private SqlConnection connection = new SqlConnection(GetConnectionString());
+        private int nr = Convert.ToInt32(ConfigurationManager.AppSettings["ChildNumberOfColumns"]);
+        private TextBox[] textBoxes;
+        private Label[] labels;
+
+        public Form1()
+        {
+            InitializeComponent();
+            populatePanel();
+            parentTable.SelectionChanged += new EventHandler(fillChildren);
+            childTable.SelectionChanged += new EventHandler(fillTextBoxes);
+            fillParent();
+           
+        }
+
+        private static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["cn"].ConnectionString.ToString();
+        }
+
+        public void fillParent()
+        {
+            string select = ConfigurationManager.AppSettings["SelectParent"];
+            da.SelectCommand = new SqlCommand(select, connection);
+            ds.Clear();
+            da.Fill(ds);
+            parentTable.DataSource = ds.Tables[0];
+        }
+
+        private void populatePanel()
+        {
+            textBoxes = new TextBox[nr];
+            labels = new Label[nr];
+
+            for (int i = 0; i < nr; i++)
+            {
+                textBoxes[i] = new TextBox();
+                textBoxes[i].Size = new System.Drawing.Size(130, 51);
+                textBoxes[i].Text = "";
+                labels[i] = new Label();
+                labels[i].Text = columnNames[i];
+            }
+
+            for (int i = 0; i < nr; i++)
+            {
+                flowLayoutPanel.Controls.Add(labels[i]);
+                flowLayoutPanel.Controls.Add(textBoxes[i]);
+            }
+        }
+
+
+        private void fillChildren(object sender, EventArgs e)
+        {
+            int parentId = (int)parentTable.CurrentRow.Cells[0].Value;
+            string select = ConfigurationManager.AppSettings["SelectChild"];
+            SqlCommand cmd = new SqlCommand(select, connection);
+            cmd.Parameters.AddWithValue("@id", parentId);
+            SqlDataAdapter daChild = new SqlDataAdapter(cmd);
+            DataSet dataSet = new DataSet();
+
+            daChild.Fill(dataSet);
+            childTable.DataSource = dataSet.Tables[0];
+        }
+
+        private void fillTextBoxes(object sender, EventArgs e)
+        {
+            for (int i = 0; i < nr; i++)
+                textBoxes[i].Text = Convert.ToString(childTable.CurrentRow.Cells[i + 1].Value);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("insert into " + childTableName + " ( " + ConfigurationManager.AppSettings["ChildLabelNames"] + " ) values ( " + columnNamesInsertParameters + " )", connection);
+                for (int i = 0; i < nr; i++)
+                {
+                    cmd.Parameters.AddWithValue(paramsNames[i], textBoxes[i].Text);
+                }
+                SqlDataAdapter daChild = new SqlDataAdapter(cmd);
+                DataSet dataSet = new DataSet();
+                connection.Open();
+                daChild.Fill(dataSet);
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                connection.Close();
+            }
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string delete = ConfigurationManager.AppSettings["DeleteChild"];
+                SqlCommand cmd = new SqlCommand(delete, connection);
+                cmd.Parameters.AddWithValue("@id", (int)childTable.CurrentRow.Cells[0].Value);
+                SqlDataAdapter daChild = new SqlDataAdapter(cmd);
+                DataSet dataSet = new DataSet();
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                daChild.Fill(dataSet);
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                connection.Close();
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string update = ConfigurationManager.AppSettings["UpdateQuery"];
+                SqlCommand cmd = new SqlCommand(update, connection);
+                for (int i = 0; i < nr; i++)
+                {
+                    cmd.Parameters.AddWithValue(paramsNames[i], textBoxes[i].Text);
+                }
+                cmd.Parameters.AddWithValue("@id", (int)childTable.CurrentRow.Cells[0].Value);
+                SqlDataAdapter daChild = new SqlDataAdapter(cmd);
+                DataSet dataSet = new DataSet();
+                connection.Open();
+                daChild.Fill(dataSet);
+                connection.Close();
+         
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                connection.Close();
+            }
+        }
+    }
+
+}
+
